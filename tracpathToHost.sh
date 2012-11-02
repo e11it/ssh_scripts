90b42d9fa2a6628c44ea3cecc8d737f3582b3733
e11it <makarov.tula@gmail.com>
2012-11-02 13:39:37 +0400

    log cleaner, output file to var

--- tracpathToHost.sh
+++ tracpathToHost.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+# http://goo.gl/C3UVr
+#
+OUTPUT_FILE=/tmp/trace_to_$1.log
+
+if [ -f $OUTPUT_FILE ]; then
+  echo "" > $OUTPUT_FILE
+fi
 
 while true
   do
     TIME=`date +%T`
     TRAC=`tracepath $1`
-    echo -e "[$TIME]\n$TRAC\n" >> /tmp/trac.log
-    tail -n 12  /tmp/trac.log
+    echo -e "[$TIME]\n$TRAC\n" >> $OUTPUT_FILE
+    tail -n 12  $OUTPUT_FILE
     sleep 3;
   done
