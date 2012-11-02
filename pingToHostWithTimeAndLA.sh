e52796bf5f9534875c323e6b849a86f48ab3f3ec
e11it <makarov.tula@gmail.com>
2012-11-02 12:24:42 +0400

    add ip address to the name of output file

--- pingToHostWithTimeAndLA.sh
+++ pingToHostWithTimeAndLA.sh
@@ -4,7 +4,7 @@ while true
         TIME=`date +%T`
         PING=`ping $1 -c 1 | grep '=2'`
 	LA=`uptime | grep -P -o "load average: (.+?)$"`
-            echo "[$TIME][$LA] $PING" >>/tmp/pingtime.log
-            tail -n 1 /tmp/pingtime.log
+            echo "[$TIME][$LA] $PING" >>/tmp/ping_to_$1.log
+            tail -n 1 /tmp/ping_to_$1.log
     sleep 1
     done
